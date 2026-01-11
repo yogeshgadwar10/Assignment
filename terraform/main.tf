19916ca4cdf6c42f40b7eed13bd0d837c481fc7a
57549_YOGESH <yogeshgadwar@gmail.com>
2026-01-11 15:47:29 +0530

    2nd-commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,3 @@
-
-# provider "aws" {
-#   region = "ap-south-1"
-# }
-
-# module "eks" {
-#   source          = "terraform-aws-modules/eks/aws"
-#   cluster_name    = "aws_eks_cluster.cbz_cluster.name"
-#   cluster_version = "1.29"
-#   subnet_ids      = data.aws_subnets.default.ids
-#   vpc_id          = "vpc-xxxx"
-
-#   eks_managed_node_groups = {
-#     default = {
-#       desired_size = 2
-#       instance_types = ["t3.medium"]
-#     }
-#   }
-# }
 terraform {
     backend "s3" {
         bucket = "Hello-World-infra" 
@@ -31,20 +12,27 @@ provider "aws" {
 
 
 module "eks" {
-    source = "./modules/eks"
-    
-    project            = var.eks_project
-    desired_nodes      = var.eks_desired_nodes
-    max_nodes          = var.eks_max_nodes
-    min_nodes          = var.eks_min_nodes
-    node_instance_type = var.eks_node_instance_type
-    environment        = var.environment
-    depends_on =    [module.rds]
-}
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
-module "s3" {
-    source = "./modules/s3"
-    
-    bucket_name  = var.s3_bucket_name
-    environment  = var.s3_environment
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = var.subnet_ids
+  vpc_id          = var.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
 }
